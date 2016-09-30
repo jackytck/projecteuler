6d8f87dcde046108ab6fdb74abe5bc618ff515a7
Jacky Tang <jackytck@gmail.com>
2016-09-30 12:00:00 +0800

    + p16

---
+++ 16/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+	"strconv"
+)
+
+func exp2(n uint) *big.Int {
+	b := big.NewInt(1)
+	b.Lsh(b, n)
+	return b
+}
+
+func powerDigitSum(n uint) int {
+	sum := 0
+	p := exp2(n)
+	for _, v := range p.Text(10) {
+		i, _ := strconv.Atoi(string(v))
+		sum += i
+	}
+	return sum
+}
+
+func main() {
+	fmt.Println(powerDigitSum(15))
+	fmt.Println(powerDigitSum(1000))
+}
+
+// Sum of digits of 2^n.

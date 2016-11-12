907a22315ad7a7c44cf32493c56cc8d40b358b70
Jacky Tang <jackytck@gmail.com>
2016-11-12 09:45:54 +0800

    + p59

---
+++ 59/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+
+	"github.com/jackytck/projecteuler/tools"
+)
+
+func read(path string) []int {
+	var ret []int
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	slice := strings.Split(strings.TrimSpace(string(data)), ",")
+	for _, v := range slice {
+		i, _ := strconv.Atoi(v)
+		ret = append(ret, i)
+	}
+	return ret
+}
+
+func keys() chan string {
+	c := make(chan string)
+	go func() {
+		for i := 'a'; i <= 'z'; i++ {
+			for j := 'a'; j <= 'z'; j++ {
+				for k := 'a'; k <= 'z'; k++ {
+					c <- string(i) + string(j) + string(k)
+				}
+			}
+		}
+		close(c)
+	}()
+	return c
+}
+
+func decrypt(text []int, key string) (string, []int) {
+	var decoded []string
+	var ascii []int
+	for i, v := range text {
+		k := key[i%3]
+		w := v ^ int(k)
+		decoded = append(decoded, string(w))
+		ascii = append(ascii, w)
+	}
+	return strings.Join(decoded, ""), ascii
+}
+
+func score(text string) int {
+	var cnt int
+	for _, v := range text {
+		if v == ' ' {
+			cnt++
+		}
+	}
+	return cnt
+}
+
+func main() {
+	var key, raw string
+	var maxScore int
+	var ascii []int
+	encrypted := read("./p059_cipher.txt")
+	for k := range keys() {
+		trial, num := decrypt(encrypted, k)
+		if s := score(trial); s > maxScore {
+			maxScore = s
+			key = k
+			raw = trial
+			ascii = num
+		}
+	}
+	fmt.Println(raw)
+	fmt.Println(key, maxScore)
+	fmt.Println(tools.Sum(ascii...))
+}
+
+// Decrypt the message and find the sum of the ASCII values in the original text.
+// Note:
+// All possible keys are tested. The best one is picked based on the number of
+// occurrences of the space character in the deciphered text.

e2adb0ffe374b5b1361040b9b701ee6a7da6610e
Jacky Tang <jackytck@gmail.com>
2016-11-26 12:33:42 +0800

    :sparkles: add Sqrt

--- tools/math.go
+++ tools/math.go
@@ -564,3 +564,25 @@ func GCD(x, y int) int {
 	}
 	return x
 }
+
+// Sqrt computes the square root of n with precision number of digits.
+// e.g. Sqrt(2, 20) => 14142135623730950488
+func Sqrt(n, precision int) *big.Int {
+	limit := Exp(10, precision+1)
+	a := big.NewInt(5 * int64(n))
+	b := big.NewInt(5)
+	ten := big.NewInt(10)
+	hundred := big.NewInt(100)
+	fortyFive := big.NewInt(45)
+	for b.Cmp(limit) < 0 {
+		if a.Cmp(b) >= 0 {
+			a.Sub(a, b)
+			b.Add(b, ten)
+		} else {
+			a.Mul(a, hundred)
+			b.Mul(b, ten)
+			b.Sub(b, fortyFive)
+		}
+	}
+	return b.Div(b, hundred)
+}

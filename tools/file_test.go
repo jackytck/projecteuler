084714498692ceab8a6d393bc1521a7d3986e2a0
Jacky Tang <jackytck@gmail.com>
2017-02-24 13:06:37 +0800

    :white_check_mark: add TestReadFile

--- tools/file_test.go
+++ tools/file_test.go
@@ -7,6 +7,39 @@ import (
 	"testing"
 )
 
+func TestReadFile(t *testing.T) {
+	cases := []struct {
+		in  string
+		out []string
+	}{
+		{"first line\nsecond line\nthird line",
+			[]string{"first line", "second line", "third line"}},
+		{"first line\nsecond line\nthird line\n",
+			[]string{"first line", "second line", "third line"}},
+	}
+	tmp := "tmp-input.txt"
+	for _, c := range cases {
+		ioutil.WriteFile(tmp, []byte(c.in), 0644)
+		lines, e := ReadFile(tmp)
+		if e != nil {
+			t.Errorf("ReadFile: %v\tExpected: nil", e)
+		}
+		if len(lines) != len(c.out) {
+			t.Errorf("ReadFile: len(%v)\tExpected: len(%v)", len(lines), len(c.out))
+		}
+		for i, v := range lines {
+			if v != c.out[i] {
+				t.Errorf("ReadFile: %v\tExpected: %v", v, c.out[i])
+			}
+		}
+	}
+	os.Remove(tmp)
+	_, err := ReadFile("not_existed.txt")
+	if err == nil {
+		t.Error("ReadFile should return error if file does not exist.")
+	}
+}
+
 func TestReadWords(t *testing.T) {
 	cases := []struct {
 		in  string

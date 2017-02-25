d99855a472a1241f2b4a9934bec47fd0e0a40148
Jacky Tang <jackytck@gmail.com>
2017-02-25 17:29:02 +0800

    :white_check_mark: add TestReadMatrix

--- tools/file_test.go
+++ tools/file_test.go
@@ -71,3 +71,47 @@ func TestReadWords(t *testing.T) {
 		t.Error("ReadWords should return error if file does not exist.")
 	}
 }
+
+func TestReadMatrix(t *testing.T) {
+	cases := []struct {
+		in  string
+		out [][]int
+	}{
+		{"131,673,234,103,18\n201,96,342,965,150\n" +
+			"630,803,746,422,111\n537,699,497,121,956\n" +
+			"805,732,524,37,331",
+			[][]int{{131, 673, 234, 103, 18}, {201, 96, 342, 965, 150},
+				{630, 803, 746, 422, 111}, {537, 699, 497, 121, 956},
+				{805, 732, 524, 37, 331}}},
+		{"131,673,234,103,18\n201,96,342,965,150\n",
+			[][]int{{131, 673, 234, 103, 18}, {201, 96, 342, 965, 150}}},
+		{"131,673,234,103,18\n201,96,342,965,150,\n",
+			[][]int{{131, 673, 234, 103, 18}, {201, 96, 342, 965, 150}}},
+	}
+	tmp := "tmp-input.txt"
+	for _, c := range cases {
+		ioutil.WriteFile(tmp, []byte(c.in), 0644)
+		mat, e := ReadMatrix(tmp)
+		if e != nil {
+			t.Errorf("ReadMatrix: %v\tExpected: nil", e)
+		}
+		if len(mat) != len(c.out) {
+			t.Errorf("ReadMatrix: len(%v)\tExpected: len(%v)", len(mat), len(c.out))
+		}
+		for i, v := range mat {
+			if len(v) != len(c.out[i]) {
+				t.Errorf("ReadMatrix: len(%v)\tExpected: len(%v)", len(v), len(c.out[i]))
+			}
+			for j, u := range v {
+				if u != c.out[i][j] {
+					t.Errorf("ReadMatrix: %v\tExpected: %v", u, c.out[i][j])
+				}
+			}
+		}
+	}
+	os.Remove(tmp)
+	_, err := ReadMatrix("not_existed.txt")
+	if err == nil {
+		t.Error("ReadMatrix should return error if file does not exist.")
+	}
+}
